78bafd049480916f54752555ea793b8425407659
jampoolg <jampoolgarcia@gmail.com>
2023-07-22 08:32:39 -0400

    create btn bar with styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,6 +11,11 @@ export default component$(() => {
     <>
       <span class="text-2xl">Buscador Simple</span>
       <span class="text-9xl">{ pokemonId }</span>
+
+      <div class="mt-2">
+        <button class="btn btn-primary mr-2">Back</button>
+        <button class="btn btn-primary mr-2">Next</button>
+      </div>
     </>
   );
 });
