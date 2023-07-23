03cf72ae61918800060fcc4f481fdb65f2767eea
jampoolg <jampoolgarcia@gmail.com>
2023-07-23 00:44:33 -0400

    added hut and loaded

--- src/components/shared/pokemons/pokemon-image.tsx
+++ src/components/shared/pokemons/pokemon-image.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 
 interface Props {
     id: number;
@@ -9,17 +9,29 @@ interface Props {
 export const PokemonImage = component$(({ id, size = 200, backImg = false }: Props) =>{
    // declaraciones
    const url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
-//    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
+   const imgLoaded = useSignal<boolean>(false);
 
    // funciones
+   useTask$(({track}) => {
+    track(() => id);
+    imgLoaded.value = false;
+   }) 
 
     // template
     return (
         <>
-          <img 
+        <div class="flex items-center justify-center" style={{ width: `${size}px`, height: `${size}px` }}>
+            { !imgLoaded.value && <span>Cargando...</span>}
+            <img 
             src={`${url}/${backImg ? 'back/': ''}${id}.png`} 
             alt="pokemon img" 
-            style={{ width: `${size}px`, height: `${size}px` }} />
+            style={{ width: `${size}px`, height: `${size}px`}}
+            class={{
+                'hidden': !imgLoaded.value
+            }}
+            onLoad$={() => imgLoaded.value=true }/>
+        </div>
+          
         </>
     )
 })
