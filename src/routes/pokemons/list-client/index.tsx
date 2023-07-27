ac131127f7467bc4da6c68b179658b905b5764b1
jampoolg <jampoolgarcia@gmail.com>
2023-07-27 00:00:44 -0400

    create state and event list-client component

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,8 +1,47 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore } from '@builder.io/qwik';
 import { DocumentHead } from '@builder.io/qwik-city';
+import { ISmallPokemon } from '~/interfaces';
+
+export interface IPokemonPageState {
+  currentPage: number;
+  pokemons: ISmallPokemon[];
+}
 
 export default component$(() => {
-  return (<><h1>client-list</h1></>)
+
+  const pokemonState = useStore<IPokemonPageState>({
+    currentPage: 0,
+    pokemons: []
+  })
+
+  return ( <>
+    <div class="flex flex-col">
+      <span class="my-5 text-5xl">status</span>
+      <span>página Actual:  { pokemonState.currentPage } </span>
+      <span>Cargando la página: </span>
+    </div>
+
+    <div class="mt-5">
+      <button 
+      onClick$={ () => pokemonState.currentPage-- }
+      class="btn btn-primary mr-2 disabled">Anterior</button>
+
+      <button 
+      onClick$={ () => pokemonState.currentPage++ }
+      class="btn btn-primary mr-2">Siguiente</button>
+    </div>
+
+    <div class="grid grid-cols-5 mt-5">
+        {/* {
+          pokemons.value.map(({ name, id }) => (
+            <div key={name} class="mt-5 flex flex-col justify-center items-center">
+              <PokemonImage id={id} />
+              <span class="capitalize">{ name }</span>
+            </div>
+          ))
+        } */}
+    </div>
+  </>)
 });
 
 export const head: DocumentHead = {
