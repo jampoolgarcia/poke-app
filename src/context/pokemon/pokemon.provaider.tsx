8ea61b5921d3c97beade8ebc1091da29c25e0709
jampoolg <jampoolgarcia@gmail.com>
2023-07-29 14:25:24 -0400

    create localstrore read and save

--- src/context/pokemon/pokemon.provaider.tsx
+++ src/context/pokemon/pokemon.provaider.tsx
@@ -1,4 +1,4 @@
-import { Slot, component$, useContextProvider, useStore } from '@builder.io/qwik';
+import { Slot, component$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import { IPokemonGameState, PokemonGameContext } from './pokemon-game.context';
 import { IPokemonListState, pokemonListContext } from './pokemon-list.context';
 
@@ -22,6 +22,32 @@ export const PokemonProvaider = component$(() => {
     // proveemos los contextos en nuestra app
   useContextProvider(PokemonGameContext, pokemonGame);
   useContextProvider(pokemonListContext, pokemonList);
+
+  // leer datos del localstorage y guardar en el context
+  // nota: al no especificar el track solo se ejecuta una vez la tarea.
+  useVisibleTask$(() => {
+    if(localStorage.getItem('pokemon-game')){
+      const {
+        isVisible,
+        isBack,
+        id,
+        size
+      } = JSON.parse(localStorage.getItem('pokemon-game')!) as IPokemonGameState;
+
+      pokemonGame.isVisible = isVisible;
+      pokemonGame.isBack = isBack;
+      pokemonGame.id = id;
+      pokemonGame.size = size;
+    }
+  })
+
+  // guardar los datos del contexto en el local storage
+  useVisibleTask$(({track}) => {
+    // ejecutar la tarea cada que cambie uno de los valores...
+    track (()=> [pokemonGame.isBack, pokemonGame.id, pokemonGame.isVisible, pokemonGame.size]);
+    // guarda en el local stora el nuevo estado 
+    localStorage.setItem('pokemon-game', JSON.stringify(pokemonGame));
+  })
 
   return (<Slot />)
 });
