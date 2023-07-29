ec01e4f9446628770b071362eff0bcdd45c9dc21
jampoolg <jampoolgarcia@gmail.com>
2023-07-29 13:19:17 -0400

    create pokemon-game.context

---
+++ src/context/pokemon/pokemon-game.context.tsx
@@ -0,0 +1,11 @@
+import { createContextId } from '@builder.io/qwik';
+
+export interface IPokemonGameState {
+    id: number;
+    size?: number;
+    isBack?: boolean;
+    isVisible?: boolean;
+}
+
+export const PokemonGameContext = createContextId('pokemon-game.context');
+
